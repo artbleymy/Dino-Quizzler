94b6390747ecc6242e9ca7a48ed860dc07780afb
Stanislav Kozlov <bleymy@mail.ru>
2019-07-28 11:45:15 +0300

    Initial Commit

---
+++ Dino Quizzler/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Dino Quizzler
+//
+//  Created by Stanislav on 28/07/2019.
+//  Copyright © 2019 Stanislav Kozlov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
